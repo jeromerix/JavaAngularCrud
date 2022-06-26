4c9b5cba164856f5d4847947988e69ea2a18f6e6
jeromerix <gamerjerome@gmail.com>
2022-06-26 22:38:19 +0200

    rollen gefixed

--- src/main/java/com/crud/springbootjwtauthorization/repository/UserRepository.java
+++ src/main/java/com/crud/springbootjwtauthorization/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.crud.springbootjwtauthorization.repository;
-
+import com.crud.springbootjwtauthorization.model.Role;
 import com.crud.springbootjwtauthorization.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import javax.management.relation.Role;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,9 +12,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     //find users by name
     Optional<User> findByUserName(String username);
 
-    @Modifying
-
     //custom query
+    @Modifying
     @Query("update User set role = :role where username = :username")
         void updateUserRole(@Param("username")String username, @Param("role") Role role);
 
