64295e8e67d5164adb4afbde8046626eb4e89b48
jeromerix <gamerjerome@gmail.com>
2022-07-06 23:30:26 +0200

    user auth added, bezig met SecurityConfig uitzoeken

---
+++ src/main/java/com/crud/springbootjwtauthorization/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.crud.springbootjwtauthorization.security;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+}
