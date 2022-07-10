515e75d9c27cfc54ee89e9d74dad63f0e3cdce15
jeromerix <gamerjerome@gmail.com>
2022-07-11 00:49:01 +0200

    security config aan het bewerken

--- src/main/java/com/crud/springbootjwtauthorization/security/SecurityConfig.java
+++ src/main/java/com/crud/springbootjwtauthorization/security/SecurityConfig.java
@@ -2,16 +2,37 @@ package com.crud.springbootjwtauthorization.security;
 
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-    //TODO : hier moet security komen
+    //TODO : nog afmaken
 
+    private CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService)
+    {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+       auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 }
