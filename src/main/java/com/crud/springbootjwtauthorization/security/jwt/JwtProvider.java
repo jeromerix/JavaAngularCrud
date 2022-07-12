7c39a2f4a52c45aba088a7b9831e95e5433a7ab2
jeromerix <gamerjerome@gmail.com>
2022-07-12 23:24:48 +0200

    keys en encryptie geupdate

--- src/main/java/com/crud/springbootjwtauthorization/security/jwt/JwtProvider.java
+++ src/main/java/com/crud/springbootjwtauthorization/security/jwt/JwtProvider.java
@@ -1,7 +1,14 @@
 package com.crud.springbootjwtauthorization.security.jwt;
 
 import com.crud.springbootjwtauthorization.security.UserPrincipal;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import javax.servlet.http.HttpServletRequest;
 
 public interface JwtProvider {
     String generateToken(UserPrincipal auth);
+
+    UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request);
+
+    boolean isTokenValid(HttpServletRequest request);
 }
--- src/main/java/com/crud/springbootjwtauthorization/security/jwt/JwtProviderImpl.java
+++ src/main/java/com/crud/springbootjwtauthorization/security/jwt/JwtProviderImpl.java
@@ -1,17 +1,25 @@
+package com.crud.springbootjwtauthorization.security.jwt;
 
 import com.crud.springbootjwtauthorization.security.UserPrincipal;
-import com.crud.springbootjwtauthorization.security.jwt.JwtProvider;
+
 import java.security.Key;
 
+import com.crud.springbootjwtauthorization.utils.SecurityUtils;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -37,5 +45,69 @@ public class JwtProviderImpl implements JwtProvider {
                 .setExpiration(new Date((System.currentTimeMillis()+ JWT_EXPIRATION_IN_MS)))
                 .signWith(key, SignatureAlgorithm.ES256)
                 .compact();
+    }
+
+    @Override
+    public UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request)
+    {
+        Claims claims = extractClaims(request);
+
+                if(claims == null)
+                {
+                    return null;
+                }
+
+                String username = claims.getSubject();
+                Long userId = claims.get("userId", Long.class);
+
+        Set<GrantedAuthority> authorities = Arrays.stream(claims.get("roles").toString().split(","))
+                .map(SecurityUtils::convertToAuthority)
+                .collect(Collectors.toSet());
+
+        UserDetails userDetails = UserPrincipal.builder()
+            .username(username)
+            .authorities(authorities)
+            .id(userId)
+            .build();
+
+        if(username == null)
+        {
+            return null;
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+    }
+
+    @Override
+    public boolean isTokenValid(HttpServletRequest request)
+    {
+        Claims claims = extractClaims(request);
+
+        if(claims == null)
+        {
+            return false;
+        }
+        if(claims.getExpiration().before(new Date()))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private Claims extractClaims(HttpServletRequest request)
+    {
+        String token = SecurityUtils.extractAuthTokenFromRequest(request);
+
+        if(token == null)
+        {
+            return null;
+        }
+
+        Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
